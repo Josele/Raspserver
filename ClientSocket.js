aeab5c666cb154a56562b2ec517e09a342ef0eb3
Josele <jl.moragas@hotmail.es>
2015-11-07 14:25:43 +0000

    socket-http server v1 working

--- ClientSocket.js
+++ ClientSocket.js
@@ -1,7 +1,8 @@
 var net = require('net');
-var str ="Hello, Im";
-
+var str ="quit";
 var client = new net.Socket();
+var cuenta = 0;
+
 client.connect(45000, '127.0.0.1', function() {
 
 	console.log('Connected');
@@ -10,11 +11,16 @@ client.connect(45000, '127.0.0.1', function() {
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-client.write(str);
-
+	client.write(str);
+	if(cuenta>1)
 	client.destroy(); // kill client after server's response
+	cuenta++;
 });
 
 client.on('close', function() {
 	console.log('Connection closed');
 });
+
+
+
+
--- controllers/ClientSocket.js
+++ controllers/ClientSocket.js
@@ -15,22 +15,36 @@ exports.answer=function(req,res){ // req will have the command from out
 if(req.query.comando.length>0){
 var str = req.query.comando;
 var client = new net.Socket();
-
+var cuenta =0;
 client.connect(45000, '127.0.0.1', function() {
 	console.log('Connected');
 	client.write(String.fromCharCode(str.length));
 });
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.write(str);
-	res.send('Received: '+ data);
-	client.destroy(); // kill client after server's response
+
+client.on('error',function(e){
+
+console.log("One error: "+ e+"\n El error ECONNRESET viene dado por cerrar abruptamente el otro socket");
+client.end(); 
+
 });
 
-client.on('close', function() {
-	console.log('Connection closed');
-});}
-else
-res.send('Null command');
 
+
+client.on('data', function(data) {
+        console.log('Received: ' + data);
+        client.write(str);
+        if(cuenta>1){
+	res.send('Received: '+ data);
+        client.end(); // kill gently the client after the second server's response
+	}
+	cuenta++;
+});
+
+client.on('end', function() {
+        console.log('Connection closed');
+	cuenta=0;
+});
+
+}else  //at here we handle a null command, the program in c isn't prepared for this.
+res.send('Null command');
 };
