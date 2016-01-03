072351a0d1b7c33dcb831e2238f824b0367b9fc0
Josele <jl.moragas@hotmail.es>
2016-01-03 00:59:54 +0000

    Async task support

--- controllers/ClientSocket.js
+++ controllers/ClientSocket.js
@@ -1,4 +1,5 @@
 var net = require('net');
+async = require("async");
 
 exports.question=function(req,res){
 res.render('prtlsviews/commandview');
@@ -14,9 +15,10 @@ exports.answer=function(req,res){ // req will have the command from out
 if(req.query.comando.length>0){
 var str = req.query.comando;
 var client = new net.Socket();
-
-
-
+var data_send;
+async.series([
+        //Load user to get userId first
+        function(callback) {
 
 client.connect({path: '/home/pi/LselRemasterizado/socket'}, function() {
 	console.log('Server: Connected and send command');
@@ -41,15 +43,21 @@ client.on('error',function(e){
 
 client.on('data', function(data) {
         console.log('Received: '+  data);
-	res.render('prtlsviews/show',{respuesta: data});
-        client.end(); // kill gently the client after the second server's response
+	//	res.render('prtlsviews/show',{respuesta: data});
+        data_send=data;
+	client.end(); // kill gently the client after the second server's response
 	
 });
 
 client.on('end', function() {
         console.log('S: Connection closed');
+callback();
 });
-
+}], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+        if (err) return next(err);
+        //Here locals will be populated with 'user' and 'posts'
+        res.render('prtlsviews/show',{respuesta: data_send});
+    });
 }else  //at here we handle a null command, the program in c isn't prepared for this.
 //res.send('Null command');
  res.render('prtlsviews/show',{respuesta: 'Null command'});
