16eb24406e2b6ceb76efc5c4159d9ed739dc53b0
Josele <jl.moragas@hotmail.es>
2016-01-03 01:08:23 +0000

    android async task too

--- controllers/ClientSocket.js
+++ controllers/ClientSocket.js
@@ -70,9 +70,10 @@ exports.answer_android=function(req,res){ // req will have the command from out
 if(req.query.comando.length>0){
 var str = req.query.comando;
 var client = new net.Socket();
-
-
-
+var data_send;
+async.series([
+        //Load user to get userId first
+        function(callback) {
 
 client.connect({path: '/home/pi/LselRemasterizado/socket'}, function() {
         console.log('S: Connected');
@@ -95,15 +96,21 @@ client.on('error',function(e){
 
 client.on('data', function(data) {
         console.log('Received: '+  data);
-        res.send(data.toString());
-        client.end(); // kill gently the client after the second server's response
+//      res.send(data.toString());
+	data_send=data;
+  	client.end(); // kill gently the client after the second server's response
 
 });
 
 client.on('end', function() {
         console.log('S: Connection closed');
+callback();
 });
-
+}], function(err) { //This function gets called after the two tasks have called$
+        if (err) return next(err);
+        //Here locals will be populated with 'user' and 'posts'
+        res.send( data_send.toString());
+    });
 }else  //at here we handle a null command, the program in c isn't prepared for this.
 res.send('Null command');
 // res.render('prtlsviews/show',{respuesta: 'Null command'});
